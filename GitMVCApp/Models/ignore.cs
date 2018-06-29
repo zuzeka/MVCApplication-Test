b9bfb42d9370fdce2965b28caeba82ae198ce83a
Zuzeka Zimba <zuzizimba@gmail.com>
2018-06-29 10:53:18 +0200

    class to be added to the git.ignore file(not to be tracked)

---
+++ GitMVCApp/Models/ignore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitMVCApp.Models
+{
+    public class ignore
+    {
+        //class not ot be tracked in git
+    }
+}
